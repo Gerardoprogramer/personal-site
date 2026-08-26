8096d8f1ffdf6653e442a1c3a895c1b02fd83977
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2026-08-26 08:34:12 -0600

    feat: update about section

--- components/sections/About.tsx
+++ components/sections/About.tsx
@@ -1,5 +1,4 @@
 import { AboutText } from "./about/AboutText ";
-import { CodeSnippetCard } from "./about/CodeSnippetCard";
 import { Reveal } from "../shared/Reveal";
 import { BackgroundLetter } from "../shared/BackgroundLetter";
 
@@ -10,12 +9,9 @@ export const About = () => {
             className="relative border-y border-border bg-surface/40 py-24"
         >
             <BackgroundLetter letter="E" />
-            <div className="mx-auto grid max-w-6xl gap-12 px-6 lg:grid-cols-[1fr_1fr] lg:gap-16">
+            <div className="mx-auto max-w-6xl px-6">
                 <Reveal>
                     <AboutText />
-                </Reveal>
-                <Reveal delay={120} className="lg:pt-14 min-w-0">
-                    <CodeSnippetCard />
                 </Reveal>
             </div>
         </section>
